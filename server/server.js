05880f735ad7d975872fe64b60633d8a816ef076
Rey Padilla <padilla.reyj@gmail.com>
2020-07-16 09:03:38 +0700

    Updated nice login form

--- server/server.js
+++ server/server.js
@@ -11064,6 +11064,7 @@ Thanks for using Polis!
 
   function handle_GET_twitterBtn(req, res) {
     let dest = req.p.dest || "/inbox";
+
     dest = encodeURIComponent(getServerNameWithProtocol(req) + dest);
     let returnUrl = getServerNameWithProtocol(req) + "/api/v3/twitter_oauth_callback?owner=" + req.p.owner + "&dest=" + dest;
     console.log('return URL');
@@ -11093,7 +11094,7 @@ Thanks for using Polis!
       },
       json: true 
     };
-
+    let errorMessage = ""
     request(options)
     .then(function (response) {
         // POST succeeded...
@@ -11196,7 +11197,7 @@ Thanks for using Polis!
           'Content-Type': 'text/html',
         });
         console.log('Invalid username or password');
-        errorMessage = "<div>Invalid Username or password</div>"
+        errorMessage = "<div style=color:red>Invalid Username or password. <a href='javascript:void(0)' onclick='window.history.back();'>Try Again</div>"
         res.status(200).send(errorMessage);
 
       }
@@ -11204,7 +11205,10 @@ Thanks for using Polis!
     .catch(function (err) {
         // POST failed...
       console.log(err);
-      res.status(200).send("error");
+      
+    
+      errorMessage = "<div style=color:red>Invalid Username or password. <a href='javascript:void(0)' onclick='window.history.back();'>Try Again</div>"
+      res.status(200).send(errorMessage);
     });
 
      
@@ -11229,12 +11233,159 @@ Thanks for using Polis!
      '</head>' +
      "<body style='max-width:320px; font-family: Futura, Helvetica, sans-serif;'>" +
       
-      `<form method="post" action="/api/v3/careLogin">
-        <div>Username <input name="username" type="text" value=""></div>
-        <div>Password <input name="password" type="password" value=""></div>
-        <div><input type="submit" value="Login"></div>
-        <input type="hidden" name="action" value="login">
-      </form>
+      `
+      <style>
+      body {
+  font: 13px/20px "Lucida Grande", Tahoma, Verdana, sans-serif;
+  color: #404040;
+  background: #0ca3d2;
+}
+
+.login {
+  position: relative;
+  margin: 30px auto;
+  padding: 20px 20px 20px;
+  width: 100%;
+  background: white;
+  border-radius: 3px;
+  -webkit-box-shadow: 0 0 200px rgba(255, 255, 255, 0.5), 0 1px 2px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 0 200px rgba(255, 255, 255, 0.5), 0 1px 2px rgba(0, 0, 0, 0.3);
+}
+
+.login:before {
+  content: '';
+  position: absolute;
+  top: -8px;
+  right: -8px;
+  bottom: -8px;
+  left: -8px;
+  z-index: -1;
+  background: rgba(0, 0, 0, 0.08);
+  border-radius: 4px;
+}
+
+.login h1 {
+  margin: -20px -20px 21px;
+  line-height: 40px;
+  font-size: 15px;
+  font-weight: bold;
+  color: #555;
+  text-align: center;
+  text-shadow: 0 1px white;
+  background: #f3f3f3;
+  border-bottom: 1px solid #cfcfcf;
+  border-radius: 3px 3px 0 0;
+  background-image: -webkit-linear-gradient(top, whiteffd, #eef2f5);
+  background-image: -moz-linear-gradient(top, whiteffd, #eef2f5);
+  background-image: -o-linear-gradient(top, whiteffd, #eef2f5);
+  background-image: linear-gradient(to bottom, whiteffd, #eef2f5);
+  -webkit-box-shadow: 0 1px whitesmoke;
+  box-shadow: 0 1px whitesmoke;
+}
+
+.login p {
+  margin: 20px 0 0;
+}
+
+.login p:first-child {
+  margin-top: 0;
+}
+
+.login input[type=text], .login input[type=password] {
+  width: 278px;
+}
+
+ 
+
+.login p.submit {
+  text-align: right;
+}
+
+.login-help {
+  margin: 20px 0;
+  font-size: 11px;
+  color: white;
+  text-align: center;
+  text-shadow: 0 1px #2a85a1;
+}
+
+.login-help a {
+  color: #cce7fa;
+  text-decoration: none;
+}
+
+.login-help a:hover {
+  text-decoration: underline;
+}
+
+:-moz-placeholder {
+  color: #c9c9c9 !important;
+  font-size: 13px;
+}
+
+::-webkit-input-placeholder {
+  color: #ccc;
+  font-size: 13px;
+}
+
+input {
+  font-family: 'Lucida Grande', Tahoma, Verdana, sans-serif;
+  font-size: 14px;
+}
+
+input[type=text], input[type=password] {
+  margin: 5px;
+  padding: 0 10px;
+  width: 200px;
+  height: 34px;
+  color: #404040;
+  background: white;
+  border: 1px solid;
+  border-color: #c4c4c4 #d1d1d1 #d4d4d4;
+  border-radius: 2px;
+  outline: 5px solid #eff4f7;
+  -moz-outline-radius: 3px;
+  -webkit-box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.12);
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.12);
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  border-color: #7dc9e2;
+  outline-color: #dceefc;
+  outline-offset: 0;
+}
+
+input[type=submit] {
+  padding: 0 18px;
+  height: 29px;
+  font-size: 12px;
+  font-weight: bold;
+  color: #527881;
+  text-shadow: 0 1px #e3f1f1;
+  background: #cde5ef;
+  border: 1px solid;
+  border-color: #b4ccce #b3c0c8 #9eb9c2;
+  border-radius: 16px;
+  outline: 0;
+  
+  </style>
+
+      <div class="login">
+        <h1>Login using CARE</h1>
+         <form method="post" action="/api/v3/careLogin">
+          <p><input type="text" name="username" value="" placeholder="Username"></p>
+          <p><input type="password" name="password" value="" placeholder="Password"></p>
+          
+          <p class="submit"><input type="submit" name="commit" value="Login"></p>
+        </form>
+      </div>
+
+      <div class="login-help">
+        <p>Forgot your password? <a href="https://care.or.th/password-reset/" target="_blank">Click here to reset it</a>.</p>
+      </div>
+
+    
+    
       `
       +
     
